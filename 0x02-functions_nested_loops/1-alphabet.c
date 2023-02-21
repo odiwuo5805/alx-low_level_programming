d295accc12c5d7d052352a6784f78ed1e4eb8a81
DANCAN AKALL <122867975+odiwuo5805@users.noreply.github.com>
2023-02-21 17:20:09 +0300

    TASK 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lowecase alphabet letters
+ *
+ */
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
