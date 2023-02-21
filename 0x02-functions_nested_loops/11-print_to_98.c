714503acfe705cf481cae01646343f6bdcaafc64
DANCAN AKALL <122867975+odiwuo5805@users.noreply.github.com>
2023-02-21 17:36:10 +0300

    TASK 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - check number from n to 98
+ * @n: integer n
+ * Description: function that print all natural numbers
+ * Return: none
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
