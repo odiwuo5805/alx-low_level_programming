8e350605f7b7f84f6f0acbe1c8d72acb94bdf61a
odiwuo5805 <odiwuordancun5805@gmail.com>
2023-03-23 08:51:36 -0700

    lib

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,18 +1,18 @@
-#ifndef _VARIADIC_FUNCTION_H_
-#define _VARIADIC_FUNCTION_H_
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
-
-struct type
+/**
+* struct printer - printer
+* @symbol: data type
+* @print: pointer
+*/
+typedef struct printer
 {
-char *character;
-void (*printf)(va_list);
-};
-typedef struct type variable_type;
-
-int _putchar(char c);
+char *symbol;
+void (*print)(va_list arg);
+} printer_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 #endif
