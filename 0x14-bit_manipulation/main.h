b44836f50ce161f4181c2438be3238f5d99d8f9c
odiwuo5805 <odiwuordancun5805@gmail.com>
2023-05-04 14:48:47 -0700

    taskS:

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 /*
- * File: holberton.h
+ * File:
  * AUth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x13-bit_manipulation directory.
