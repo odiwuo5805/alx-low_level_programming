40a6fc828b9ac0ebf8a9e547fd632b700de6ab59
odiwuo5805 <odiwuordancun5805@gmail.com>
2023-05-04 14:44:43 -0700

    task

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * get_bit - Get value of a bit at a given index
+ *
+ * @n: long int
+ *
+ * @index: unsigned int
+ *
+ * Return: Bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 57)
+		return (-1);
+	return ((n >> index) & 1);
+}
+
